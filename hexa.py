620870fa072c098c8ce6d308f837c89551e7c427
Syed Musfar <63565699+Musfarr@users.noreply.github.com>
2022-09-02 21:53:26 +0500

    add hexa decimal program

---
+++ hexa.py
@@ -0,0 +1,12 @@
+
+
+#python program to find the values of hexadeximal numbers
+
+
+#here i created dictionaries
+d1 = {"a": "10", "b": "11", "c": "12", "d": "13" , "e":"14" ,"f":"15"}
+
+
+print(d1.keys())
+a = input(" choose any one hexadecimal Alphabet to find value in decimal\n")
+print(d1[a])
